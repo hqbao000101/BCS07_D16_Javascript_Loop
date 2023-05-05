481cefefe6c3332e663ac41022e5c4abbc6e7929
hqbao000101 <hbao592000@gmail.com>
2023-05-05 13:58:39 +0700

    setup: add-comment-requests

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("testing...");
+// ! EX1: Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + 3 +...+ n > 10000 
+// ! EX2: Viết chương trình nhập vào 2 số x, n tính tổng S(n) = x + x^2 + x^3 +...+ x^n (Sử dụng vòng lặp và hàm) 
+// ! EX3: Nhập vào n. Tính giai thừa 1 * 2 *...* n 
+// ! EX4: Hãy viết chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu div vào vị trí chẵn thì bg màu đỏ, lẻ thì bg màu xanh dương 
+// ! EX5: Viết chương trình có một ô input, một button. Khi click vào button thì in ra các số nguyên tố từ 1 tới giá trị của ô input 
