8642695b0f37e1df9f54ff55e94ef318759d3f1f
hqbao000101 <hbao592000@gmail.com>
2023-05-08 16:57:08 +0700

    feat: ex1-ex2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-// ! EX1: Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + 3 +...+ n > 10000 
+// ! EX1: Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + 3 +...+ n > 10000
+/* 
+todo: Mô Hình 3 Khối
+* input: không có
+* bước xử lý: 
+*   1. Tạo một biến sum giữ kết quả tổng các giá trị từ 1 đến n và một biến result để chứa kết quả số n cần tìm
+*   2. Chạy 1 vòng lặp với i tăng dần từ 1 và điều kiện của vòng lặp sẽ là biến tổng các giá trị sum <= 10000
+*   3. Trong vòng lặp sẽ cho biến sum cộng dồn giá trị i ở mỗi vòng lặp; biến result sẽ lấy giá trị i ở mỗi vòng lặp
+*   4. Khi giá trị sum lớn hơn 10000, vòng lặp sẽ kết thúc và biến result sẽ mang giá trị của biến i lớn nhất mà sum <= 10000 cũng là giá trị n mà ta cần tìm 
+* output: số n thoả đề bài và in ra màn hình
+*/ 
+
 function ex1() {
   var sum = 0;
   var result = 0;
@@ -9,7 +20,72 @@ function ex1() {
   document.querySelector(".ex1 .result span:nth-child(2)").innerHTML = result;
 }
 
-// ! EX2: Viết chương trình nhập vào 2 số x, n tính tổng S(n) = x + x^2 + x^3 +...+ x^n (Sử dụng vòng lặp và hàm) 
-// ! EX3: Nhập vào n. Tính giai thừa 1 * 2 *...* n 
-// ! EX4: Hãy viết chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu div vào vị trí chẵn thì bg màu đỏ, lẻ thì bg màu xanh dương 
-// ! EX5: Viết chương trình có một ô input, một button. Khi click vào button thì in ra các số nguyên tố từ 1 tới giá trị của ô input 
+// ! EX2: Viết chương trình nhập vào 2 số x, n tính tổng S(n) = x + x^2 + x^3 +...+ x^n (Sử dụng vòng lặp và hàm)
+/* 
+todo: Mô Hình 3 Khối
+* input: Số x và số n
+* bước xử lý: 
+*   1. Tạo sự kiện onclick ở button
+*   2. Lấy số x và số n người dùng nhập vào
+*   2. Tạo biến chứa kết quả result = 0
+*   3. Chạy vòng lặp với biến i tăng dần từ 1 đến n và điều kiện chạy là i <= n
+*   4. Trong vòng lặp thực hiện cộng dồn biến result với Math.pow(số x, biến i ở mỗi vòng lặp) <=> result += x^i
+* output: tổng s(n) theo số x và số n và in ra màn hình
+*/ 
+
+function ex2() {
+  // todo: take user data
+  var x_number = +document.getElementById("X").value;
+  var n_number = +document.getElementById("N").value;
+
+  // todo: loop
+  var result = 0;
+  for (var i = 1; i <= n_number; i++) {
+    result += Math.pow(x_number, i);
+  }
+
+  // todo: print result
+  document.querySelector(".ex2 .result .text-success").innerHTML = result;
+}
+
+// ! EX3: Nhập vào n. Tính giai thừa 1 * 2 *...* n
+/* 
+todo: Mô Hình 3 Khối
+* input: 
+* bước xử lý: 
+*   1. 
+*   2. 
+*   3. 
+*   4. 
+*   5. 
+* output: 
+*/ 
+
+
+// ! EX4: Hãy viết chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu div vào vị trí chẵn thì bg màu đỏ, lẻ thì bg màu xanh dương
+/* 
+todo: Mô Hình 3 Khối
+* input:
+* bước xử lý: 
+*   1. 
+*   2. 
+*   3. 
+*   4. 
+*   5. 
+* output: 
+*/ 
+
+
+// ! EX5: Viết chương trình có một ô input, một button. Khi click vào button thì in ra các số nguyên tố từ 1 tới giá trị của ô input
+/* 
+todo: Mô Hình 3 Khối
+* input:
+* bước xử lý: 
+*   1. 
+*   2. 
+*   3. 
+*   4. 
+*   5. 
+* output: 
+*/ 
+
